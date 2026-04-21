a27623b4cddc828c2d1f25b3a9c187c74575a9cf
wladimirrs <185645045+wladimirrs@users.noreply.github.com>
2026-04-21 12:34:10 +0200

    Datenbankverbindung erstellt

---
+++ core/config.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+
+const TIMEOUT = 60;
+
+const DB = array("host" => "localhost",
+                "dbname" => "jsproj",
+                "user" => "root",
+                "pwd" => ""
+                
+                
+                );
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 require_once("core/config.php");
 
+$dsn = "mysql:host=".DB["host"].";dbname=".DB["dbname"].";charset=utf8";
 
-
+$pdo = new PDO($dsn, DB["user"], DB["pwd"]);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Fehlermode & ERRMODE_EXCEPTION
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); // Fetchmode & Assoziatives Array (Assoc)
 
 
 ?>

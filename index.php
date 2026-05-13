0c9fe0d5c0e1be01d139dd2d883dbb3614f0c587
wladimirrs <185645045+wladimirrs@users.noreply.github.com>
2026-05-13 16:30:01 +0200

    DB-Verbindung mit PDO-Objekt

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
 require_once("core/config.php");
+// config in diesem Fall Front-Controller (Sessionstart und Laden der Einstellungen als Erstes)
+// Dependency Injection: alle Objekte an einem Ort initiiert und bei Bedarf ineinander injiziert
+
+$dsn = "mysql:host="   .   DB["host"]   .   ";dbname="   .   DB["name"];
+// entspricht: mysql:host=localhost;dbname=jsproj;
+$pdo = new PDO($dsn, DB["user"], DB["pwd"]);
+// Verbindungsobjekt erstellen mit den Eingaben
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Bei Fehler Exception statt nur Warnung
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+// Ergebnisse als Dictionary auslesen
+
+
+
+
+
+
+?>

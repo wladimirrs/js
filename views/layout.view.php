b343a86308a6ae09c62882d1d5a2814ecfc503a0
wladimirrs <185645045+wladimirrs@users.noreply.github.com>
2026-05-18 13:21:29 +0200

    Im Layout Liste aus Seitenname und Seitenzahl, für jedes li ein Link mit entsprechender nav

---
+++ views/layout.view.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rezepte</title>
+    <link rel="stylesheet" href="css/main.css">
+    <meta property="og:title" content="Meine Rezepte">
+    <meta name="keywords" content="Rezepte, PHP, SQL">
+</head>
+
+
+
+
+
+
+
+
+
+
+
+<body>
+    <nav class="main-nav">
+        <?php // Liste aus Titel + id
+            $nav_links = ["Home" => "1", "Seite 1" => "5", "Benutzer" => "6", "Seite 3" => "7", "Seite 4" => "8", "Seite 5" => "9"];
+            $current_nav = $_GET["nav"] ?? 1;?>
+
+        <div>
+            <h2 class="nav-title">Kochbuch</h2>
+        </div>
+
+        <ul>
+            <?php foreach ($nav_links as $title => $nav_id): ?> <!-- List-Item für jeden Array-Eintrag, mit jeweils Link und nav entsprechend der Liste, Klasse active oder nicht -->
+                <li>
+                <a href="index.php?nav=<?= $nav_id ?>" class="nav-link <?= ($current_nav == $nav_id) ? "active" : "" ?>"><?= $title ?></a>
+                </li>
+                <?php endforeach; ?>
+            </ul>
+    </nav>
+
+    
+
+
+
+
+
+
+
+
+    
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
